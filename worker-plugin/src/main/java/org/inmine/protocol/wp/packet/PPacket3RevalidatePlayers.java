38894460979814be40b11178c32ec7795645ede2
xtrafrancyz <xtrafrancyz@gmail.com>
2017-11-29 00:31:14 +0200

    Dumb

--- worker-plugin/src/main/java/org/inmine/protocol/wp/packet/PPacket3RevalidatePlayers.java
+++ worker-plugin/src/main/java/org/inmine/protocol/wp/packet/PPacket3RevalidatePlayers.java
@@ -3,18 +3,16 @@ package org.inmine.protocol.wp.packet;
 import org.inmine.network.Buffer;
 import org.inmine.network.Packet;
 
-import java.util.List;
-
 /**
  * @author xtrafrancyz
  */
 public class PPacket3RevalidatePlayers extends Packet {
-    public List<String> nicknames;
+    public int[] userIds;
 
     public PPacket3RevalidatePlayers() { }
 
-    public PPacket3RevalidatePlayers(List<String> nicknames) {
-        this.nicknames = nicknames;
+    public PPacket3RevalidatePlayers(int[] userIds) {
+        this.userIds = userIds;
     }
 
     @Override
@@ -24,11 +22,11 @@ public class PPacket3RevalidatePlayers extends Packet {
 
     @Override
     public void write(Buffer buffer) {
-        buffer.writeStringList(nicknames);
+        buffer.writeIntArray(userIds, buffer::writeInt);
     }
 
     @Override
     public void read(Buffer buffer) {
-        nicknames = buffer.readStringList(500, 40);
+        userIds = buffer.readIntArray(500, buffer::readInt);
     }
 }
