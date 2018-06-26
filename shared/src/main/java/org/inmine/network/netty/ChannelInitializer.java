6c8f4553927c591ecd8d48cbccde9737d2283289
xtrafrancyz <xtrafrancyz@gmail.com>
2018-06-26 19:01:59 +0300

    Optimize pipeline

--- shared/src/main/java/org/inmine/network/netty/ChannelInitializer.java
+++ shared/src/main/java/org/inmine/network/netty/ChannelInitializer.java
@@ -15,7 +15,6 @@ import java.util.logging.Logger;
 public class ChannelInitializer<T extends Channel> extends io.netty.channel.ChannelInitializer<T> {
 
     private final NetworkStatisticsImpl statistics;
-    private final PacketEncoder encoder;
     private final PacketRegistry packetRegistry;
     private final Supplier<NettyPacketHandler> packetHandlerGenerator;
     private final Logger logger;
@@ -25,15 +24,13 @@ public class ChannelInitializer<T extends Channel> extends io.netty.channel.Chan
         this.packetRegistry = packetRegistry;
         this.packetHandlerGenerator = packetHandlerGenerator;
         this.logger = logger;
-        this.encoder = new PacketEncoder(statistics);
     }
 
     @Override
     protected void initChannel(T ch) throws Exception {
         ch.config().setAllocator(PooledByteBufAllocator.DEFAULT);
 
-        ch.pipeline().addLast("packet-encoder", this.encoder);
-        ch.pipeline().addLast("packet-decoder", new PacketDecoder(this.statistics, this.packetRegistry));
+        ch.pipeline().addLast("encoder-decoder", new PacketCodec(this.statistics, this.packetRegistry));
         HandlerBoss boss = new HandlerBoss(this.packetHandlerGenerator.get(), this.logger);
         ch.pipeline().addLast("boss", boss);
         ch.attr(HandlerBoss.BOSS_KEY).set(boss);
--- shared/src/main/java/org/inmine/network/netty/PacketCodec.java
+++ shared/src/main/java/org/inmine/network/netty/PacketCodec.java
@@ -2,7 +2,7 @@ package org.inmine.network.netty;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ByteToMessageCodec;
 import io.netty.handler.codec.DecoderException;
 
 import org.inmine.network.NetworkStatisticsImpl;
@@ -12,16 +12,38 @@ import org.inmine.network.PacketRegistry;
 import java.util.List;
 
 /**
- * Created by RINES on 17.11.17.
+ * @author xtrafrancyz
  */
-public class PacketDecoder extends ByteToMessageDecoder {
+public class PacketCodec extends ByteToMessageCodec<Packet> {
 
     private final NetworkStatisticsImpl statistics;
     private final PacketRegistry packetRegistry;
 
-    public PacketDecoder(NetworkStatisticsImpl statistics, PacketRegistry packetRegistry) {
+    public PacketCodec(NetworkStatisticsImpl statistics, PacketRegistry packetRegistry) {
         this.statistics = statistics;
         this.packetRegistry = packetRegistry;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Packet packet, ByteBuf out) throws Exception {
+        ByteBuf temp = ctx.alloc().buffer();
+        NettyBuffer buffer = NettyBuffer.newInstance(temp);
+        try {
+            buffer.writeSignedVarInt(packet.getId());
+            packet.write(buffer);
+
+            int start = out.writerIndex();
+
+            buffer.setHandle(out);
+            buffer.writeVarInt(temp.readableBytes());
+            out.writeBytes(temp);
+
+            statistics.sentPackets().addAndGet(1);
+            statistics.sentBytes().addAndGet(out.writerIndex() - start);
+        } finally {
+            buffer.release();
+            temp.release();
+        }
     }
 
     @Override
